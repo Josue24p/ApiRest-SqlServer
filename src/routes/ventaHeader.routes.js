5db0525142fe2267debc94d66c6cb40a83f40c9c
Lonely <josuepoza.24@gmail.com>
2024-03-15 02:15:54 -0500

    Agregue la inserción a la BD
    Código para insertar los datos a la BD y mostrar en consola y postman.

--- src/routes/ventaHeader.routes.js
+++ src/routes/ventaHeader.routes.js
@@ -1,7 +1,8 @@
+//Se importa router de la librería express
 import { Router } from "express";
-
+//importamos las funciones que tiene ventaHeaderController
 import { getVentasHeader, getVentasHeaderById, createNewVentaH } from "../controllers/ventaheader.controller";
-
+//declaramos una constante router que tendra todas las funcionalidades de router
 const router =  Router();
 //tengo que agregar a la app toda esta ruta 
 router.get('/ventaHeader', getVentasHeader);
