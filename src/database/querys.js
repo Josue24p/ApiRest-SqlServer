52aaaad02fb6b611cd6299711c1bacbdf24d7dbf
Lonely <josuepoza.24@gmail.com>
2024-03-18 23:21:30 -0500

    Actualización

--- src/database/querys.js
+++ src/database/querys.js
@@ -1,10 +1,10 @@
 export const queries = {
-    getAllClient: 'SELECT * FROM CLIENTES',
-    addNewClient: 
-    'INSERT INTO CLIENTES (NroDocumento, ApellidosNombres, FechaHoraRegistro) VALUES (@NroDocumento, @ApellidosNombres, @FechaHoraRegistro)',
-    getClientById: 'SELECT * FROM CLIENTES WHERE IdCliente = @Id',
-    deleteClientById: 'DELETE FROM CLIENTES WHERE IdCliente = @Id',
-    getTotalClients: 'SELECT COUNT(*) FROM CLIENTES',
-    updateCliente: 
-    'UPDATE CLIENTES SET NroDocumento = @NroDocumento, ApellidosNombres = @ApellidosNombres, FechaHoraRegistro = @FechaHoraRegistro WHERE IdCliente = @Id',
+    getAllVentasH: 'Select * from VentaHeader',
+    newVentasH: 
+    'Insert into VentaHeader (SerieNumero,FechaHora,IdCliente,Subtotal,IGV,Total) VALUES(@SerieNumero,@FechaHora,@IdCliente,@Subtotal,@IGV,@Total)',
+    getVentaHById: 'SELECT * FROM VENTAHEADER WHERE IdVentaH = @Id',
+    deleteVentaHById: 'Delete from VentaHeader where IdVentaH = @Id',
+    getTotalVentaH: 'Select count(*) from VentaHeader',
+    updateVentaH: 
+    'UPDATE VENTAHEADER SET SerieNumero=@SerieNumero,FechaHora=@FechaHora,IdCliente=@IdCliente, Subtotal=@Subtotal, IGV=@IGV, Total=@Total WHERE IdVentaH=@Id',
 }
