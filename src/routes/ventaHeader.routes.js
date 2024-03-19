80c59aed0428bd6848b2fecb837bdd0f4efd51a7
Lonely <josuepoza.24@gmail.com>
2024-03-19 01:19:38 -0500

    Acomodando

--- src/routes/ventaHeader.routes.js
+++ src/routes/ventaHeader.routes.js
@@ -1,7 +1,9 @@
 //Se importa router de la librería express
 import { Router } from "express";
 //importamos las funciones que tiene ventaHeaderController
-import { getVentasHeader, getVentasHeaderById, createNewVentaH, getTotalVentasH, deleteVentaHeaderById, updateVentaHeaderById} from "../controllers/ventaHeader.controller";
+import { getVentasHeader, getVentasHeaderById, createNewVentaH, 
+getTotalVentasH, deleteVentaHeaderById, updateVentaHeaderById}
+from "../controllers/ventaHeader.controller";
 //declaramos una constante router que tendra todas las funcionalidades de router
 const router =  Router();
 //tengo que agregar a la app toda esta ruta 
