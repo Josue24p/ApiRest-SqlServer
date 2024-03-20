b16d2957c6cc9d12638f0508d8184c47ad369b87
Lonely <josuepoza.24@gmail.com>
2024-03-19 23:24:17 -0500

    Función de las rutas Ventas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express'//importar la librerí express
 import config from './config'
 
-import { ventasHeaders } from "./routes/ventaHeader.routes";
+import  ventasHeaders  from "./routes/ventaHeader.routes";
+import ventaDetails from "./routes/ventaDetails.routes";
 
 const app = express() //app tiene el uso del express
 
@@ -14,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));//recibe datos que viene del fo
 
 
 app.use(ventasHeaders)
-
+app.use(ventaDetails)
 
 
 export default app //exportamos app
